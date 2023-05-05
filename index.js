3d2c9158ae1b7386ac7906a875bc63cee501d2f4
Hunter Wade <calebh.wade@gmail.com>
2023-05-05 17:41:05 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
